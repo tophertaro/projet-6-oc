87539a86950b56ebfa0f518f96a1580f1c30e9ea
Phorimavong Christopher <phorimavong.c@gmail.com>
2024-10-09 17:03:19 +0200

    user schema done

--- backend/app.js
+++ backend/app.js
@@ -1,2 +1,13 @@
-const express = require('express');
-const app = express();
+const express = require('express'); // import express
+const app = express(); // création de l'application express
+const mongoose = require('mongoose')
+
+
+// connection à la bdd
+mongoose.connect('mongodb+srv://luffy1:projet6oc@cluster0.zytcp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+module.exports = app; // export de l'app 
+
+
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,7 @@
-test 
+const app = require('./app');
+const http = require('http')
+
+const port = process.env.PORT || 3001; // Définition du port
+app.listen(port, () => {
+  console.log(`Le serveur tourne sur le port ${port}`);
+});
