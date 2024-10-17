c8e48665ba01a1c96a8d272c240e4f355dd92291
Phorimavong Christopher <phorimavong.c@gmail.com>
2024-10-17 12:05:30 +0200

    modèle de donnés

--- backend/app.js
+++ backend/app.js
@@ -11,33 +11,22 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
+app.get((req, res) => {
   console.log('Requête reçue !');
-  next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
-
-app.use((req, res) => {
-  console.log('Réponse envoyée avec succès !');
-});
 
 
 
 const mongoose = require('mongoose')
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 // connection à la bdd
-mongoose.connect('mongodb+srv://luffy1:projet6oc@cluster0.zytcp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Connexion à MongoDB réussie'))
+  .catch((err) => console.error('Erreur de connexion à MongoDB :', err));
 
 
 
